d00cb371f45d82de7e5d10f12caef77d7da8730f
Ronaldo-Rodrigues <Ronaldo.rodrigjunior@gmail.com>
2022-01-28 00:42:02 -0200

    Commit Inicial*
    *O jogo ja estava sendo feito quando este repositório foi criado.
    Já implementado:
    Scene Setup;
    Layers collision;
    Tilemaps (com cena montada)
    Nodes de Tilemaps
    Game Manager;
    Player Prefab;
    Player Movement;
    Player Imput;
    Passagens (triggers);
    Pallet Behaviour;

---
+++ Arch-Enemies/Assets/Scripts/GameManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ghost[] ghosts;
+
+    public Pacman pacman;
+
+    public Transform pallets;
+    public int score { get; private set; }
+    public int lives { get; private set; }
+    public int ghostMultiplier { get; private set; } = 1;
+    private void Start()
+    {
+        NewGame();
+    }
+    private void Update()
+    {
+        if (this.lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+    private void NewRound()
+    {
+        foreach(Transform pallet in this.pallets)
+        {
+            pallet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+
+        this.pacman.gameObject.SetActive(false);
+    }
+    private void ResetGhostMultiplier()
+    {
+        this.ghostMultiplier = 1;
+    }
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+    public void GhostEaten(Ghost ghost)
+    {
+        int points = ghost.points * this.ghostMultiplier;
+        SetScore(this.score + (points));
+        this.ghostMultiplier++;
+    }
+    public void PacmanEaten()
+    {
+        this.pacman.gameObject.SetActive(false);
+        
+        SetLives(this.lives - 1);
+
+        if(this.lives > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f);            
+        }
+        else { GameOver(); }
+    }
+    public void PalletEaten(Pallet pallet)
+    {
+        pallet.gameObject.SetActive(false);
+        SetScore(this.score + pallet.points);
+       //CHECA SE TODAS AS PALLETS FORAM COMIDAS PARA REINICIAR O ROUND
+        if (!HasPalletsRemaining())
+        {
+            this.pacman.gameObject.SetActive(false);
+            Invoke(nameof(NewRound), 3.0f);
+        }
+    }
+    public void PowerPallet(PowerPallet ppallet)
+    {
+        PalletEaten(ppallet);
+    }
+    public bool HasPalletsRemaining()
+    {
+        foreach(Transform pallet in this.pallets)
+        {
+            if (pallet.gameObject.activeSelf)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Arch-Enemies/Assets/Scripts/Passagem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passagem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other != null)
+        {
+            if (other.transform.position.x > 1)
+            {
+                other.transform.position = new Vector3(-13.5f, transform.position.y, transform.position.z);
+            }
+            else if(other.transform.position.x < -1) 
+            {
+                other.transform.position = new Vector3(13.5f, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
